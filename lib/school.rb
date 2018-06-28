af5867ec96e778913000984b6a511c28f1606cbf
paulheisele@gmail.com <paulheisele@gmail.com>
2018-06-28 01:24:25 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+  attr_reader :roster
+
+  def initialize(name)
+    @name= name 
+    @roster= {}
+  end 
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def add_student(name, grade)
+    if @roster.key.include?(grade)
+      @roster[grade]<<name 
+    else 
+      @roster[grade]=[]
+      @roster[grade]<<name 
+    end
+
+    def sort
+      @roster.each do |grade, students|
+        students.sort!
+      end
+      @roster
+    end
+end
